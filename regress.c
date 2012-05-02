GNOME/gobject-introspection-tests

commit ab03db3647447503b7b56cd8c6ec7b609f014861

Make introspection compile with -Wall -Werror
Address all gcc warnings, -Werror is not enabled yet but at least
-Wall -Werror passes on my machine.

regress.c
@@ -226,11 +226,12 @@ regress_test_closure_variant (GClosure *closure, const GVariant* arg)
   GValue return_value = {0, };
   GValue arguments[1] = {{0,} };
   GVariant *ret;
+  GVariant *local_arg = (GVariant*)g_memdup(arg, sizeof (GVariant*));
 
   g_value_init (&return_value, G_TYPE_VARIANT);
 
   g_value_init (&arguments[0], G_TYPE_VARIANT);
-  g_value_set_variant (&arguments[0], arg);
+  g_value_set_variant (&arguments[0], local_arg);
 
   g_closure_invoke (closure,
                     &return_value,
@@ -239,6 +240,7 @@ regress_test_closure_variant (GClosure *closure, const GVariant* arg)
 
   ret = g_value_get_variant (&return_value);
 
+  g_free (local_arg);
   g_value_unset (&return_value);
   g_value_unset (&arguments[0]);
 
@@ -2610,7 +2612,7 @@ regress_test_obj_null_out (RegressTestObj **obj)
 void
 regress_test_array_fixed_out_objects (RegressTestObj ***objs)
 {
-    RegressTestObj **values = g_new(gpointer, 2);
+    RegressTestObj **values = (RegressTestObj**)g_new(gpointer, 2);
 
     values[0] = regress_constructor();
     values[1] = regress_constructor();

gimarshallingtests.c
@@ -3826,7 +3826,7 @@ gi_marshalling_tests_object_vfunc_with_callback (GIMarshallingTestsObject *objec
 static int
 _callback (int val, void *user_data)
 {
-    g_assert(user_data == 0xdeadbeef);
+    g_assert((int)user_data == (int)0xdeadbeef);
     return val;
 }
 

commit 801a34462e37737998ffab059a3b252ee7d73e4a

Fix GVariant handling in regress_test_closure_variant
Commit ab03db36 introduced some low-level g_memdup()ing of the passed GVariant
in regress_test_closure_variant(). This causes a race condition with GI clients
like pygobject's test suite, which sometimes fail with

  (runtests.py:15653): GLib-CRITICAL **: g_variant_ref_sink: assertion `value->ref_count > 0' failed

Drop the memduping again, and instead make the argument non-const (which is
true, as the method temporarily increases the refcount).

regress.c
@@ -221,17 +221,16 @@ regress_test_closure_one_arg (GClosure *closure, int arg)
  * Return value: (transfer full): the return value of @closure
  */
 GVariant*
-regress_test_closure_variant (GClosure *closure, const GVariant* arg)
+regress_test_closure_variant (GClosure *closure, GVariant* arg)
 {
   GValue return_value = {0, };
   GValue arguments[1] = {{0,} };
   GVariant *ret;
-  GVariant *local_arg = (GVariant*)g_memdup(arg, sizeof (GVariant*));
 
   g_value_init (&return_value, G_TYPE_VARIANT);
 
   g_value_init (&arguments[0], G_TYPE_VARIANT);
-  g_value_set_variant (&arguments[0], local_arg);
+  g_value_set_variant (&arguments[0], arg);
 
   g_closure_invoke (closure,
                     &return_value,
@@ -240,7 +239,6 @@ regress_test_closure_variant (GClosure *closure, const GVariant* arg)
 
   ret = g_value_get_variant (&return_value);
 
-  g_free (local_arg);
   g_value_unset (&return_value);
   g_value_unset (&arguments[0]);
 

regress.h
@@ -122,7 +122,7 @@ GPtrArray *regress_test_garray_full_return (void);
 /* closure */
 int regress_test_closure (GClosure *closure);
 int regress_test_closure_one_arg (GClosure *closure, int arg);
-GVariant *regress_test_closure_variant (GClosure *closure, const GVariant* arg);
+GVariant *regress_test_closure_variant (GClosure *closure, GVariant* arg);
 
 /* value */
 int regress_test_int_value_arg(const GValue *v);
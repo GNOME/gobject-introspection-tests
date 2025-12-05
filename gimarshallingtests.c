GNOME/gobject-introspection-tests

commit 1cf9c86c4e79050bb089c4abc6c8ab9f68249e71

Add test case similar to gst_init

gimarshallingtests.c
@@ -2831,6 +2831,32 @@ gi_marshalling_tests_length_array_utf8_full_inout (gchar ***array_inout, size_t 
 }
 
 /**
+ * gi_marshalling_tests_length_array_utf8_optional_inout:
+ * @inout_length: (inout) (optional): @array_inout's length
+ * @array_inout: (inout) (array length=inout_length) (optional): array ["🅰", "β", "c", "d"]
+ *
+ * See https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9524
+ */
+void
+gi_marshalling_tests_length_array_utf8_optional_inout (int *inout_length, char **array_inout[])
+{
+  if (*inout_length > 0)
+    gi_marshalling_tests_length_array_utf8_full_inout (array_inout, (size_t *) inout_length);
+  else {
+    gchar **array_out = g_new0 (gchar *, 2);
+
+    g_assert_nonnull (inout_length);
+    g_assert_nonnull (array_inout);
+
+    array_out[0] = g_strdup ("a");
+    array_out[1] = g_strdup ("b");
+
+    *array_inout = array_out;
+    *inout_length = 2;
+  }
+}
+
+/**
  * gi_marshalling_tests_zero_terminated_array_utf8_none_return:
  *
  * Returns: (array zero-terminated) (transfer none):

gimarshallingtests.h
@@ -999,6 +999,9 @@ GI_TEST_EXTERN
 void gi_marshalling_tests_length_array_utf8_full_inout (gchar ***array_inout, size_t *inout_length);
 
 GI_TEST_EXTERN
+void gi_marshalling_tests_length_array_utf8_optional_inout (int *inout_length, char **array_inout[]);
+
+GI_TEST_EXTERN
 const gchar *const *gi_marshalling_tests_zero_terminated_array_utf8_none_return (void);
 
 GI_TEST_EXTERN

commit ff002f84755e0100277d593204d01603237a2cae

Fix test case of optional inout arguments
The (optional) annotation means that for (out) and (inout) parameters,
the caller can pass NULL to ignore the output parameter[0].

However the current test case dereferences the argument without
checking, and asserts that the "optional" parameters are non-null.

Adjust the test case to accept NULL, and assert that
 - either both arguments are NULL or non-NULL
 - if the array argument is non-NULL, it does not point to NULL (not nullable)

Keep the somewhat artificial behavior of returning a fixed two-element
array when called with a non-null, zero-length in-array, so existing
callers are unaffected.

This will allow using the test in implementations like gjs that pass on
the NULL value for optional arguments instead of replacing it with an
empty list (like pygobject).

Fixes: 1cf9c86 ("Add test case similar to gst_init")
[0] https://gi.readthedocs.io/en/latest/annotations/giannotations.html#type-signature

gimarshallingtests.c
@@ -2840,9 +2840,17 @@ gi_marshalling_tests_length_array_utf8_full_inout (gchar ***array_inout, size_t 
 void
 gi_marshalling_tests_length_array_utf8_optional_inout (int *inout_length, char **array_inout[])
 {
-  if (*inout_length > 0)
+  if (inout_length == NULL)
+    {
+      g_assert_null (array_inout);
+    }
+  else if (*inout_length > 0)
     {
       size_t size = *inout_length;
+
+      g_assert_nonnull (array_inout);
+      g_assert_nonnull (*array_inout);
+
       gi_marshalling_tests_length_array_utf8_full_inout (array_inout, &size);
       *inout_length = size;
     }